b3a3cf5d8841e23e025fc5e8971a5e44ba7932bb
qq1270735037 <74816245+qq1270735037@users.noreply.github.com>
2021-11-07 16:54:48 +0800

    Add files via upload

--- demo3/src/main/java/com/example/demo/Demo3Application.java
+++ demo3/src/main/java/com/example/demo/Demo3Application.java
@@ -6,21 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo3Application {
 
+
     public static final String USERNAME = "user";
     public static final String PASSWORD = "11111111";
     public static void main(String[] args) {
         SpringApplication.run(Demo3Application.class, args);
-        String token = TokenUtil.sign(USERNAME, PASSWORD);
 
-        System.out.println("加密后的token为：" + token);
-
-        boolean flag = TokenUtil.verfiy(token);
-
-        if(flag){
-            System.out.println("校验成功");
-        } else {
-            System.out.println("校验失败");
-        }
 
     }
 
--- demo3/src/main/java/com/example/demo/controller/logincontroller.java
+++ demo3/src/main/java/com/example/demo/controller/logincontroller.java
@@ -1,27 +1,69 @@
 package com.example.demo.controller;
 
+import com.example.demo.TokenUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequestMapping("")
 @Controller
 public class logincontroller {
+
+
     @RequestMapping("/login")
-    public String login(HttpServletRequest request) {
-        String u = request.getParameter("username");
-        String P =  request.getParameter("password");
-        return "6666";
+    public String login(HttpServletRequest request, HttpServletResponse httpServletResponse) {
+        String username = request.getParameter("username");
+        String password =  request.getParameter("password");
+        String token = TokenUtil.sign(username, password);
+        Cookie ck=new Cookie("token",token);
+        System.out.println("加密后的token为：" + token);
+
+        boolean flag = TokenUtil.verfiy(token);
+
+        if(flag){
+            System.out.println("校验成功");
+        } else {
+            System.out.println("校验失败");
+            return "loginfalse";
+        }
+        httpServletResponse.addCookie(ck);
+        return "index";
 
     }
     @RequestMapping("/index")
     public String index(){
         return "login";
     }
-    @RequestMapping("/")
-    public String index2(){
+    @RequestMapping("")
+    public String login(){
         return "login";
     }
+    @RequestMapping("/sysA")
+    public String sysA(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(cookies==null){
+            return "login";
+        }
+        return "sysA";
+    }
+    @RequestMapping("/sysB")
+    public String sysB(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(cookies==null){
+            return "login";
+        }
+        return "sysB";
+    }
+    @RequestMapping("/logout")
+    public String logout(HttpServletResponse httpServletResponse){
+        Cookie ck=new Cookie("token",null);
+        ck.setMaxAge(0);
+        httpServletResponse.addCookie(ck);
+        return "logout";
+    }
+
 }
